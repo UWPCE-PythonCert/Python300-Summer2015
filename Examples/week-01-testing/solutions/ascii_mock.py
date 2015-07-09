c3c82723222c739ea38f80d681190ae73f2ce2b2
junkmob <gregcorradini@gmail.com>
2015-07-09 13:56:32 -0700

    add ascii solutions

--- Examples/week-01-testing/solutions/ascii_mock.py
+++ Examples/week-01-testing/solutions/ascii_mock.py
@@ -46,15 +46,59 @@ class TestAscii2Ord( unittest.TestCase ):
     def test_convert_all_every_result(self):
         pass
 
-    def test_convert_all_missing_result(self):
+    def test_convert_all_nothing_was_called(self):
         #
         #  harder to write, let's move on
         #  NOTE: we need two more tests here
         #  1. we need a test that asserts nothing was called
+        #
+        # ARRANGE
+        original_method = Ascii2Ord.to_ordinal
+        Ascii2Ord.to_ordinal = MagicMock(return_value=True)
+
+        # ACT
+        Ascii2Ord().convert_all('abc')
+
+        # ASSERT
+        self.assertEqual(Ascii2Ord.to_ordinal.call_count,3)
+        Ascii2Ord.to_ordinal = original_method
+
+    def test_convert_all_return_value_None(self):
+        #
+        #  harder to write, let's move on
+        #  NOTE: we need two more tests here
         #  2. we need a test that asserts None return value
+        #
+        # ARRANGE
+        original_method = Ascii2Ord.to_ordinal
+        Ascii2Ord.to_ordinal = MagicMock(return_value=None)
+
+        # ACT
+        results = Ascii2Ord().convert_all('junk')
+
+        # ASSERT
+        self.assertEqual(Ascii2Ord.to_ordinal.call_count,4)
+        self.assertEqual(results,[])
+        Ascii2Ord.to_ordinal = original_method
+
+    def test_convert_all_return_multiple_side_effects(self):
+        #
+        #  harder to write, let's move on
+        #  NOTE: we need two more tests here
         #  3. let's look at multiple side effects
         #
-        pass
+        # ARRANGE
+        original_method = Ascii2Ord.to_ordinal
+        Ascii2Ord.to_ordinal = MagicMock(side_effect=[97,97,None,None])
+
+        # ACT
+        results = Ascii2Ord().convert_all('junk')
+
+        # ASSERT
+        self.assertEqual(Ascii2Ord.to_ordinal.call_count,4)
+        self.assertEqual(results,[97,97])
+        Ascii2Ord.to_ordinal = original_method
+
 
     def test_convert_all_missing_index_error(self):
         pass
