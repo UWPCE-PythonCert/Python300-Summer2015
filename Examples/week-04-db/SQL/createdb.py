81e5d9a09e5fb515787c163cb91467110db573b8
junkmob <gregcorradini@gmail.com>
2015-07-23 19:38:34 -0700

    update createdb for Anand

--- Examples/week-04-db/SQL/createdb.py
+++ Examples/week-04-db/SQL/createdb.py
@@ -1,16 +1,27 @@
 import os
 import sqlite3
+from utils import show_table_metadata
 
-DB_FILENAME = 'books.db'
+DB_FILENAME = 'book.db'
+SCHEMA_FILENAME = 'ddl.sql'
 DB_IS_NEW = not os.path.exists(DB_FILENAME)
 
 def main():
-    conn =  sqlite3.connect(DB_FILENAME)
-    if DB_IS_NEW:
-        print 'Need to create database and schema'
-    else:
-        print 'Database exists, assume schema does, too.'
-    conn.close()
+    with sqlite3.connect(DB_FILENAME) as conn:
+        #conn =  sqlite3.connect(DB_FILENAME)
+        if DB_IS_NEW:
+            print 'Creating schema'
+            with open(SCHEMA_FILENAME, 'rt') as f:
+                schema = f.read()
+                conn.executescript(schema)
+        else:
+            print "Database exists, introspecting:"
+            tablenames = ['author', 'book']
+            cursor = conn.cursor()
+            for name in tablenames:
+                print "\n"
+                show_table_metadata(cursor, name)
+        #conn.close()
 
 if __name__ == '__main__':
     main()
