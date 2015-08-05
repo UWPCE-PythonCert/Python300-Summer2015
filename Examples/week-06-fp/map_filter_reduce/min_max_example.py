9cbcd8bebaeb9f68fa14a3b7810c68788ab66493
junkmob <gregcorradini@gmail.com>
2015-08-05 08:59:31 -0700

    add get_max fp

--- Examples/week-06-fp/map_filter_reduce/min_max_example.py
+++ Examples/week-06-fp/map_filter_reduce/min_max_example.py
@@ -44,27 +44,30 @@ lt = lambda x,y: x < y
 default = lambda item: item or 0
 gt_comparitor = lambda accum,curr: (gt(accum,curr) and accum) or curr
 lt_comparitor = lambda accum,curr: (lt(accum,curr) and accum) or curr
-def get_min(items):
-    return reduce(
-        lt_comparitor,
-        items
-    )
+def get_max(items):
+    try:
+        return [reduce(
+            gt_comparitor,
+            items
+        )]
+    except TypeError:
+        return []
+
 
 if __name__ == '__main__':
-
     print "[ RUNNING ]: get_min tests..."
-    assert get_min([0,1,3,5]) == 0
-    assert get_min([-100,0,1,2]) == -100
-    assert get_min(['a','b','c']) == 'a'
-    assert get_min(['\x00','Z','-']) == '\x00'
+    assert get_min([0,1,3,5]) == [0]
+    assert get_min([-100,0,1,2]) == [-100]
+    assert get_min(['a','b','c']) == ['a']
+    assert get_min(['\x00','Z','-']) == ['\x00']
     assert get_min([]) == []
     print "[ PASSED ]: get_min assertions passed!"
 
     print "[ RUNNING ]: get_max tests..."
-    assert get_max([5,2,0,1]) == 5
-    assert get_max([-100,0,1,2]) == 2
-    assert get_max(['a','b','c']) == 'c'
-    assert get_max(['\x00','Z','-']) == 'Z'
+    assert get_max([5,2,0,1]) == [5]
+    assert get_max([-100,0,1,2]) == [2]
+    assert get_max(['a','b','c']) == ['c']
+    assert get_max(['\x00','Z','-']) == ['Z']
     assert get_max([]) == []
     print "[ PASSED ]: get_max assertions passed!"
 
