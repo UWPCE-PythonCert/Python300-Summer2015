a8bb74e851aefdede906f0e4f24eababd9213108
junkmob <gregcorradini@gmail.com>
2015-08-06 16:40:27 -0700

    update min max examples

---
+++ Examples/week-06-fp/solutions/min_max_example.py
@@ -0,0 +1,59 @@
+
+def get_min(items):
+    '''
+    pluck the min value from the list
+
+    :param items: list of items
+    :return: list with min item
+    if no min determined an empty list is returned
+    '''
+    min_result = None
+    for item in items:
+        if min_result is None:
+            # set it
+            min_result = item
+            continue
+
+        if item < min_result:
+            min_result = item
+    return [] if min_result is None else [min_result]
+
+
+def get_max(items):
+    '''
+    pluck the max value from the list
+
+    :param items: list of items
+    :return: list with max item
+    if no max determined an empty list is returned
+    '''
+    max_result = None
+    for item in items:
+        if max_result is None:
+            # set it
+            max_result = item
+            continue
+
+        if item > max_result:
+            max_result = item
+    return [] if max_result is None else [max_result]
+
+
+if __name__ == '__main__':
+    print "[ RUNNING ]: get_min tests..."
+    assert get_min([0,1,3,5]) == [0]
+    assert get_min([-100,0,1,2]) == [-100]
+    assert get_min(['a','b','c']) == ['a']
+    assert get_min(['\x00','Z','-']) == ['\x00']
+    assert get_min([]) == []
+    print "[ PASSED ]: get_min assertions passed!"
+
+    print "[ RUNNING ]: get_max tests..."
+    assert get_max([5,2,0,1]) == [5]
+    assert get_max([-100,0,1,2]) == [2]
+    assert get_max(['a','b','c']) == ['c']
+    assert get_max(['\x00','Z','-']) == ['Z']
+    assert get_max([]) == []
+    print "[ PASSED ]: get_max assertions passed!"
+
+
