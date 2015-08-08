9582cad5f13dba6f0d4e771fc63a1bbfa7753014
junkmob <gregcorradini@gmail.com>
2015-08-08 09:17:41 -0700

    add multipledispatch method from book

---
+++ Examples/week-06-fp/multipledispatch/models.py
@@ -0,0 +1,4 @@
+class Thing(object): pass
+class Paper(Thing): pass
+class Scissors(Thing): pass
+class Rock(Thing): pass
---
+++ Examples/week-06-fp/multipledispatch/play.py
@@ -0,0 +1,22 @@
+from models import Paper,Scissors,Rock
+from draw import draw
+import random
+
+if __name__ == '__main__':
+    options = [Paper,Scissors,Rock]
+    for i in range(1000):
+        player1 = random.choice(options)
+        player2 = random.choice(options)
+        print "[ PLAYER 1 ]: plays %s" % player1.__name__
+        print "[ PLAYER 2 ]: plays %s" % player2.__name__
+        winner = draw(player1(),player2())
+
+        winner_is_player_1 = True
+        if isinstance(winner,player2):
+            winner_is_player_1 = False
+
+        print "[ WINNER ]: %s with %s\n------\n" % (
+            'player1' if winner_is_player_1 else 'player2',
+            winner.__class__.__name__
+        )
+
