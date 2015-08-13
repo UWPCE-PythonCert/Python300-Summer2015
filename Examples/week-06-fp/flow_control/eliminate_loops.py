2ebcae1edcf236866363f95a0f6e2cee79d7ff0d
junkmob <gregcorradini@gmail.com>
2015-08-13 08:31:14 -0700

    add some documentation and flesh out a little bit

--- Examples/week-06-fp/flow_control/eliminate_loops.py
+++ Examples/week-06-fp/flow_control/eliminate_loops.py
@@ -1,12 +1,25 @@
 
-def running_sum(total,numbers):
-    total = total + numbers[0]
+def running_sum(start,numbers):
+    '''
+    return the sum of the numbers list
+
+    >>> running_sum(0,[0,1,2,3])
+    6
+    >>> running_sum(1,[0,1,2,3])
+    7
+
+    :param total:
+    :param numbers:
+    :return: summed integer
+    '''
+    total = start + numbers[0]
     for current_index,value in enumerate(numbers,1):
         if current_index == len(numbers):
             return total
         total += numbers[current_index]
 
 if __name__ == '__main__':
-    final = running_sum(0,range(11))
-    print "[ FINAL VALUE ]: %s" % final
-    assert running_sum(0,range(11)) == sum(range(11))
+    assert running_sum(0,range(11)) == sum(range(11),0)
+    assert running_sum(1,range(4))  == sum(range(4),1)
+    assert running_sum(0,range(1))  == sum(range(1),0)
+    print "[ PASSED ]: all assertions met..."
--- Examples/week-06-fp/solutions/eliminate_loops.py
+++ Examples/week-06-fp/solutions/eliminate_loops.py
@@ -1,18 +1,21 @@
 
-def running_sum(total,numbers):
-    total = total + numbers[0]
+def running_sum(start,numbers):
+    total = start + numbers[0]
     for current_index,value in enumerate(numbers,1):
         if current_index == len(numbers):
             return total
         total += numbers[current_index]
 
-def running_sum(total,current_numbers):
+def running_sum(start,current_numbers):
     if len(current_numbers) == 0:
-        return total
-    total = current_numbers[0] + total
+        #print "[ STOP ]: returning %i" % start
+        return start
+    #print "[ ADDING ]: %i += %i" % (start,current_numbers[0])
+    total = start + current_numbers[0]
+    #print "[ NEW TOTAL ]: %i" % total
     return running_sum(total, current_numbers[1:])
 
 if __name__ == '__main__':
-    final = running_sum(0,range(11))
-    print "[ FINAL VALUE ]: %s" % final
-    assert running_sum(0,range(11)) == sum(range(11))
+    assert running_sum(0,range(11)) == sum(range(11),0)
+    assert running_sum(1,range(4))  == sum(range(4),1)
+    assert running_sum(0,range(1))  == sum(range(1),0)
